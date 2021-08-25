5557507613ad949a8bb735e5157475a66056d95f
Shahroze Husain <shahroze93@gmail.com>
2021-08-25 13:58:36 -0400

    updating CreatePost

--- db/migrate/20210825162739_create_posts.rb
+++ db/migrate/20210825162739_create_posts.rb
@@ -2,6 +2,9 @@ class CreatePosts < ActiveRecord::Migration[6.1]
   def change
     create_table :posts do |t|
       t.string :name
+      t.text :img_url
+      t.text :link_url
+      t.text :description
       t.references :user, null: false, foreign_key: true
 
       t.timestamps
