5c1cb953f76744cfbf83ec92c5ab19201f565502
Shahroze Husain <shahroze93@gmail.com>
2021-08-25 20:12:12 -0400

    updated UserPosts and added username to posts

--- client/src/screens/UserPosts/UserPosts.jsx
+++ client/src/screens/UserPosts/UserPosts.jsx
@@ -9,15 +9,18 @@ export default function UserPosts(props) {
       {posts.map((post) => (
         <div key={post.id}>
           {currentUser?.id === post.user_id && (
-            <section>
-            <p>{post.name}</p>
+          <section>
+          <Link to={`/posts/${post.id}`}>
+            <h4>{post.name}</h4>
+            <img src={post.img_url} alt={post.name} />
+          </Link>
             <div>
               <Link to={`/posts/${post.id}/edit`}>
                 <button>Edit</button>
               </Link>
               <button onClick={() => handleDelete(post.id)}>Delete</button>
               </div>
-              </section>
+        </section>
       )}
       </div>
       ))}
