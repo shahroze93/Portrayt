a7aca8329946c54ec1830f92fb4124aa96ae740b
Shahroze Husain <shahroze93@gmail.com>
2021-08-26 08:58:37 -0400

    debugging segments

--- client/src/screens/PostCreate/PostCreate.jsx
+++ client/src/screens/PostCreate/PostCreate.jsx
@@ -1,69 +1,72 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 export default function PostCreate(props) {
   const [formData, setFormData] = useState({
-    name: '',
-    img_url: '',
-    link_url: '',
-    description: ''
+    name: "",
+    img_url: "",
+    link_url: "",
+    description: "",
   });
   const { name, img_url, link_url, description } = formData;
   const { handleCreate } = props;
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData((prevState) => ({
-      ...prevState,
-      [name]: value,
-    }));
-  };
-
-  const handleSegmentChange = (e) => {
-    const { value } = e.target;
-    setSelectedFlavor(value);
-  };
-
-    const handleSubmit = async (e) => {
-    e.preventDefault();
-    handleCreate(formData);
-    const AddSegment = await 
-  };
-
   return (
     <section>
       <h3>Create Post</h3>
-    <div className="newPostContainer">
-      <div className="postTitle">{formData.name}</div>
-      <img className="postIMG" src={formData.img_url} alt={formData.name} />
-      <div className="postLink">{formData.link_url}</div>
-      <div className="postDesc">{formData.description}</div>
+      <div className="newPostContainer">
+        <div className="postTitle">{formData.name}</div>
+        <img className="postIMG" src={formData.img_url} alt={formData.name} />
+        <div className="postLink">{formData.link_url}</div>
+        <div className="postDesc">{formData.description}</div>
       </div>
-    <br />
-    <form
-      // onSubmit={(e) => {
-      //   e.preventDefault();
-      //   handleCreate(formData);
-      // }}
-      onSubmit={handleSubmit}
+      <br />
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          handleCreate(formData);
+        }}
       >
-      <label>
-        Name:
-        <input type='text' name='name' value={name} onChange={handleChange} />
-      </label>
-      <label>
-        Image URL:
-        <input type='text' name='img_url' value={img_url} onChange={handleChange} />
-      </label>
-      <label>
-        Link URL to Website (optional):
-        <input type='text' name='link_url' value={link_url} onChange={handleChange} />
-      </label>
-      <label>
-        Description:
-        <input type='text' name='description' value={description} onChange={handleChange} />
-      </label>
-      <button>Submit</button>
+        <label>
+          Name:
+          <input type="text" name="name" value={name} onChange={handleChange} />
+        </label>
+        <label>
+          Image URL:
+          <input
+            type="text"
+            name="img_url"
+            value={img_url}
+            onChange={handleChange}
+          />
+        </label>
+        <label>
+          Link URL to Website (optional):
+          <input
+            type="text"
+            name="link_url"
+            value={link_url}
+            onChange={handleChange}
+          />
+        </label>
+        <label>
+          Description:
+          <input
+            type="text"
+            name="description"
+            value={description}
+            onChange={handleChange}
+          />
+        </label>
+        <select onChange={handleChange} defaultValue="default">
+          <option disabled value="default">
+            -- Select Product Segment --
+          </option>
+          {segments.map((segment) => (
+            <option value={segment.id}>{segment.name}</option>
+          ))}
+        </select>
+        <button>Submit</button>
       </form>
-      </section>
+    </section>
   );
 }
