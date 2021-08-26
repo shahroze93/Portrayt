5970c93e2b63e573a643792ba5b2100a1f51f332
Shahroze Husain <shahroze93@gmail.com>
2021-08-26 17:03:30 -0400

    mirgations carried out

--- db/migrate/20210826205336_create_comments.rb
+++ db/migrate/20210826205336_create_comments.rb
@@ -1,7 +1,7 @@
 class CreateComments < ActiveRecord::Migration[6.1]
   def change
     create_table :comments do |t|
-      t.content :text
+      t.text :content
       t.references :user, null: false, foreign_key: true
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_25_182428) do
+ActiveRecord::Schema.define(version: 2021_08_26_205748) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.text "content"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
+  create_table "comments_posts", id: false, force: :cascade do |t|
+    t.bigint "post_id", null: false
+    t.bigint "comment_id", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "name"
@@ -45,5 +58,6 @@ ActiveRecord::Schema.define(version: 2021_08_25_182428) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "comments", "users"
   add_foreign_key "posts", "users"
 end
