a34318166cf5d903194ded3a0d37254241c3da6c
Shahroze Husain <shahroze93@gmail.com>
2021-08-27 13:49:26 -0400

    Navbar set up

--- client/src/components/Layout/Layout.jsx
+++ client/src/components/Layout/Layout.jsx
@@ -1,28 +1,11 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
+import React from "react";
+import Nav from "../Nav/Nav";
 
 export default function Layout(props) {
   const { currentUser, handleLogout } = props;
   return (
     <header>
-      <Link to='/'><h1>PORTRAYT</h1></Link>
-      {currentUser ? (
-        <div>
-          <Link to={`/edit-account`}><p>{currentUser.username}</p></Link>
-        </div>
-      ) : (
-          <div>
-          <Link to='/login'>Login</Link>
-          <Link to='/signup'>Sign Up</Link>
-          </div>
-      )}
-      {currentUser && (
-        <div>
-          <Link to='/posts/new'>Create Post</Link>
-          <Link to='/myposts'>My Posts</Link>
-          <button onClick={handleLogout}>Logout</button>
-        </div>
-      )}
+      <Nav currentUser={currentUser} handleLogout={handleLogout} />
       {props.children}
     </header>
   );
