68a3ef950278a66af57bc6b200bfb61d00eb7cd9
Shahroze Husain <shahroze93@gmail.com>
2021-08-27 14:59:58 -0400

    added BE deployment link to services

--- client/src/services/api-config.js
+++ client/src/services/api-config.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const baseUrl =
   process.env.NODE_ENV === 'production'
-    ? 'deployed link goes here'
+    ? 'https://portrayt-api.herokuapp.com'
     : 'http://localhost:3000';
 
 const api = axios.create({
