0397ece7e2680671df8b41d917f128834d9acdd4
Shahroze Husain <shahroze93@gmail.com>
2021-08-29 10:47:55 -0400

    Added CSS to comment input forms

--- client/src/screens/CommentCreate/CommentCreate.jsx
+++ client/src/screens/CommentCreate/CommentCreate.jsx
@@ -26,14 +26,30 @@ export default function CommentCreate(props) {
     <div>
       <form onSubmit={handleSubmit}>
         <div>Add Comment</div>
-        <div>{currentUser?.username}</div>
+        <div className="currentUserTag">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+            />
+          </svg>
+          {currentUser?.username}
+        </div>
         <input
           type="text"
           value={content}
           name="content"
           placeholder="enter a public comment"
           onChange={handleChange}
-          className="w-9/12 border rounded-full py-3 px-3 border-green-400 placeholder-gray-500 
+          className="w-8/12 md:w-9/12 border rounded-full py-3 px-3 border-green-400 placeholder-gray-500 
           focus:outline-none focus:ring-2 focus:ring-primary-green focus:border-transparent m-3"
         />
         <button
--- client/src/screens/SignIn/SignIn.jsx
+++ client/src/screens/SignIn/SignIn.jsx
@@ -30,15 +30,15 @@ export default function SignIn(props) {
             <Link className="float-right" to="/">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                class="h-8 w-8"
+                className="h-8 w-8"
                 fill="none"
                 viewBox="0 0 24 24"
                 stroke="white"
               >
                 <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
                   d="M6 18L18 6M6 6l12 12"
                 />
               </svg>
