4171af21ec16b4692bd60c99ef0de8a7ca5999b6
Shahroze Husain <shahroze93@gmail.com>
2022-06-03 18:55:47 +0800

    Added SEO

--- client/src/screens/CategoryBanner/CategoryBanner.jsx
+++ client/src/screens/CategoryBanner/CategoryBanner.jsx
@@ -10,12 +10,12 @@ export default function CategoryBanner(props) {
       <div className="categoryBanner">
         <div className="catOptions">
           <Link className="catLink" to={`/`}>
-            <div className="categoryHeader">Discover</div>
+            <h1 className="categoryHeader">Discover</h1>
           </Link>
           {segments.map((segment) => (
             <div key={segment.id}>
               <Link className="catLink" to={`/segments/${segment.id}`}>
-                <div className="categoryHeader">{segment.name}</div>
+                <h1 className="categoryHeader">{segment.name}</h1>
               </Link>
             </div>
           ))}
